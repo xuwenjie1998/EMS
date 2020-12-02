3dc53477b7d385dade8db762cac2e24b0403ded1
xuwenjie1998 <2995414049@qq.com>
2020-12-02 15:05:15 +0800

    version 1.0.6 修复部分数据未提交

--- src/test/java/com/sawyer/ems_thymeleaf/Test.java
+++ src/test/java/com/sawyer/ems_thymeleaf/Test.java
@@ -9,6 +9,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.atomic.LongAdder;
 
 @SpringBootTest
 public class Test {
@@ -33,7 +35,8 @@ public class Test {
         Date date1 = new Date();
         System.out.println(date1.toString());
         long time = date1.getTime();
-        System.out.println(time);
+        System.out.println("time :"+time);
+        System.out.println(System.currentTimeMillis());
         java.sql.Date date = new java.sql.Date(time);
         System.out.println(date);
 
@@ -43,6 +46,53 @@ public class Test {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         String format = dateFormat.format(date1);
         System.out.println(format);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void test3(){
+        LongAdder count = new LongAdder();
+        count.add(1);
+        count.add(1);
+        count.add(1);
+        long l = count.longValue();
+        System.out.println(l);
+//        int a = 1/0;
+//        System.out.println(a);
+        int b = 0/1;
+        System.out.println(b);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void test4(){
+//        Scanner scanner = new Scanner(System.in);
+//        int i = scanner.nextInt();
+//        System.out.println(i);
+    }
+
+
+    public enum SeasonEnum {
+        SPRING(1), SUMMER(2), AUTUMN(3), WINTER(4);
+    private int seq;
+    SeasonEnum(int seq) {
+        this.seq = seq;
+    }
+
+    public int getSeq() {
+        return seq;
+
+            }
+    }
+
+    public static void main(String[] args) {
+//        SeasonEnum
+//        if (空格a == b 空格) 反例
+
+
+//        if (flag == 0) {
+//            System.out.println(say);
+//        }
+        Math.abs(1);
+        
     }
 
 }
